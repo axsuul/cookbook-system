2fbe1aaffe1d684ca09422b62fbffe3012b3fb92
James Hu <axsuul@gmail.com>
2012-09-14 13:53:56 -0700

    Change package name and use a default recipe now

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,8 @@ license           "Apache 2.0"
 description       "System related recipes"
 version           "0.0.1"
 
-recipe "system::upgrade_packages", "Upgrades apt packages"
+recipe "system", "Runs all system recipes"
+recipe "system::update_packages", "Updates apt packages"
 
 %w{ubuntu debian}.each do |os|
   supports os
